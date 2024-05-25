dbefd6634a70dd3338994bfe8ae40257ae64a602
ibalakirev <Impulseproger@gmail.com>
2024-05-26 00:29:33 +0300

    V 1.02
    Desc

--- Assets/Scripts/SequencesAnimationsText.cs
+++ Assets/Scripts/SequencesAnimationsText.cs
@@ -9,6 +9,7 @@ public class SequencesAnimationsText : MonoBehaviour
     private void Start()
     {
         float duration = 2f;
+        float delay = 2f;
         int loopValue = -1;
         string textReplaced = "Я заменил этот текст";
         string textAdded = "Я добавил этот текст";
@@ -17,7 +18,7 @@ public class SequencesAnimationsText : MonoBehaviour
         Sequence sequence = DOTween.Sequence();
 
         sequence.Append(_text.DOText(textReplaced, duration));
-        sequence.Append(_text.DOText(textAdded, duration).SetRelative());
+        sequence.Append(_text.DOText(textAdded, duration).SetRelative().SetDelay(delay));
         sequence.Append(_text.DOText(textScrambled, duration, true, ScrambleMode.All));
 
         sequence.SetLoops(loopValue);
